ccad2bb09aa2f8fabe8584a9ab07745fbd7b55d9
hector <hector.nuenez.lopez.al@iespoligonosur.org>
2026-09-24 19:27:33 +0200

    Fimal ejercicio 3

--- UD01/Clase File/Ejercicio3.java
+++ UD01/Clase File/Ejercicio3.java
@@ -1,28 +1,38 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Ejercicio3 {
-    public static void listarEstructura(File directorio) {
-        System.out.println(directorio.getName());
+
+    public static void listarDirectorio(File directorio, String sangria) {
+        System.out.println(sangria + directorio.getName());
 
         String[] contenido = directorio.list();
-        if (contenido != null) {
-            for (String nombre : contenido) {
-                File elemento = new File(directorio, nombre);
-                if (elemento.isDirectory()) {
-                    listarEstructura(elemento); 
-                    System.out.println(elemento.getName());
-                }
+        if (contenido == null) {
+            return; // no es un directorio o está vacío
+        }
+
+        for (String nombre : contenido) {
+            File elemento = new File(directorio, nombre);
+            if (elemento.isDirectory()) {
+                listarDirectorio(elemento, sangria + "\t");
+            } else {
+                System.out.println(sangria + "\t" + nombre);
             }
         }
     }
 
     public static void main(String[] args) {
-        File escritorio = new File(System.getProperty("user.home"), "Desktop");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dime la ruta del directorio que quieres listar: ");
+        String directorio = sc.nextLine();
+        File escritorio = new File(System.getProperty("user.home"));
+        File raiz = new File(escritorio, directorio);
 
-        System.out.println("--- Estructura del ejercicio 1 ---");
-        listarEstructura(new File(escritorio, "d"));
+        if (!raiz.exists()) {
+            System.out.println("No se encuentra el directorio: " + raiz.getAbsolutePath());
+            return;
+        }
 
-        System.out.println("\n--- Otra estructura de prueba ---");
-        listarEstructura(new File(escritorio, "otraCarpeta"));
+        listarDirectorio(raiz, "");
     }
 }
